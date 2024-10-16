8198467fec6fb56b52ba7e685952186095287ab3
Assumpta Mwikali <assumptamwikali2002@gmail.com>
2024-10-16 10:58:30 +0300

    initial commit

---
+++ database/migrations/2024_10_14_172859_create_menus_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained();
+            $table->String('name');
+            $table->Integer('price');
+            $table->String('description');
+            $table->String('offers');
+            $table->String('allergens');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menus');
+    }
+};
